bd056a3a9eaf61b9ab638ae3f6b41ede830b82e8
surajvns1571999 <surajvns1571999@gmail.com>
2026-05-23 12:00:23 +0530

    Response of LLM in a specific structure

--- googleAIStudio/src/main/java/com/example/googleAIStudio/controller/StructureOutputController.java
+++ googleAIStudio/src/main/java/com/example/googleAIStudio/controller/StructureOutputController.java
@@ -1,7 +1,13 @@
 package com.example.googleAIStudio.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +27,28 @@ public class StructureOutputController {
 	}
 
 	@GetMapping("/chat-structure")
-	public ResponseEntity<CountryCities> chat(@RequestParam("message") String message) {
+	public ResponseEntity<CountryCities> chatStructure(@RequestParam("message") String message) {
 		CountryCities countryCities = chatClient.prompt().user(message).call().entity(CountryCities.class);
+		return ResponseEntity.ok(countryCities);
+	}
+
+	@GetMapping("/chat-list")
+	public ResponseEntity<List<String>> chatlist(@RequestParam("message") String message) {
+		List<String> countryCities = chatClient.prompt().user(message).call().entity(new ListOutputConverter());
+		return ResponseEntity.ok(countryCities);
+	}
+
+	@GetMapping("/chat-map")
+	public ResponseEntity<Map<String, Object>> chatMap(@RequestParam("message") String message) {
+		Map<String, Object> countryCities = chatClient.prompt().user(message).call().entity(new MapOutputConverter());
+		return ResponseEntity.ok(countryCities);
+	}
+
+	@GetMapping("/chat-beanlist")
+	public ResponseEntity<List<CountryCities>> chatbeanlist(@RequestParam("message") String message) {
+		List<CountryCities> countryCities = chatClient.prompt().user(message).call()
+				.entity(new ParameterizedTypeReference<List<CountryCities>>() {
+				});
 		return ResponseEntity.ok(countryCities);
 	}
 }
