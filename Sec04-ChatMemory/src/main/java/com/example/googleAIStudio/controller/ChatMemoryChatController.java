feade8635a026fde2dd4d997d47144b61529f276
surajvns1571999 <surajvns1571999@gmail.com>
2026-06-14 11:44:28 +0530

    successfully implements key as conversation_ID

--- Sec04-ChatMemory/src/main/java/com/example/googleAIStudio/controller/ChatMemoryChatController.java
+++ Sec04-ChatMemory/src/main/java/com/example/googleAIStudio/controller/ChatMemoryChatController.java
@@ -1,11 +1,14 @@
 package com.example.googleAIStudio.controller;
 
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;;
 
 @RestController
 @RequestMapping("/api")
@@ -18,7 +21,8 @@ public class ChatMemoryChatController {
 	}
 
 	@GetMapping("/chat-memory")
-	public String chat(@RequestParam("message") String message) {
-		return chatClient.prompt().user(message).call().content();
+	public String chat(@RequestHeader("username") String username, @RequestParam("message") String message) {
+		return chatClient.prompt().user(message).advisors(advisoraspec -> advisoraspec.param(CONVERSATION_ID, username))
+				.call().content();
 	}
 }
